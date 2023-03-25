cabde79d91b7e173a926f45712d5883ce2d3ce20
Алексей Лисичников <alexochanel@gmail.com>
2023-03-25 17:30:05 +0300

    Добавьте файлы проекта.

---
+++ Laba_12/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Дорогой пользователь, введи название города для поиска индекса: ");
+            string s = Console.ReadLine();
+            // создаю значение s которое будет вводит пользователь
+
+            // создаю новый словарь
+            Hashtable dict = new Hashtable()
+            {
+                ["пенза"] = 58,
+                ["иркутск"] = 38,
+                ["липецк"] = 48,
+                ["москва"] = 50,
+                ["мурманск"] = 51,
+                ["новгород"] = 53,
+                ["орел"] = 57,
+                ["самара"] = 63,
+                ["саратов"] = 64,
+                ["сахалинская область"] = 65
+            };
+
+            // вывожу на консоль то что написал пользователь
+            Console.WriteLine($"индекс этого города: {dict[s]}");
+        }
+    }
+}
